b1572647e631a6be3b413a6d5251011ee56847e8
CHARLLYS97 <jhonnycharllys@hotmail.com>
2024-03-18 23:29:52 -0300

    implementando botões e carrinho de compra

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,23 @@
-function App() {
+import { Routes, Route } from "react-router-dom"
+import { Container } from "react-bootstrap"
+import { Home } from "./pages/Home"
+import { Store } from "./pages/Store"
+import { About } from "./pages/About"
+import { Navbar} from "./components/Navbar"
 
-  return <h1>Hi</h1>
+function App() {
+  return (
+    <>
+    <Navbar />
+    <Container className="mb-4">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/store" element={<Store />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Container>
+    </>
+  )
 }
 
 export default App
